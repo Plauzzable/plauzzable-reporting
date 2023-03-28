146fb34bf936c6c123082d09a91f89bdbdc4d95f
Austin King <austin.king@plauzzable.com>
2023-03-28 10:41:09 -0700

    chore: migrate from aws-sdk v2 to v3

--- plauz-report-new-users/src/lib/env_vars.mjs
+++ plauz-report-new-users/src/lib/env_vars.mjs
@@ -1,22 +1,24 @@
-import AWS from "aws-sdk";
-AWS.config.update({ region: "us-east-1" });
+import { DecryptCommand, KMSClient } from "@aws-sdk/client-kms";
 
 const decrypt_env_var = async (envVariable) => {
-    const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
-    const encrypted = process.env[envVariable];
-  
-    const kms = new AWS.KMS();
-    try {
-      const req = {
-        CiphertextBlob: Buffer.from(encrypted, "base64"),
-        EncryptionContext: { LambdaFunctionName: functionName },
-      };    
-      const data = await kms.decrypt(req).promise();    
-      return data.Plaintext.toString("ascii");
-    } catch (err) {
-      console.log("Decrypt error:", err);
-      throw err;
-    }
-  };
-  
-  export { decrypt_env_var };
+  const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
+  const encrypted = process.env[envVariable];
+
+  const kmsClient = new KMSClient({ region: "us-east-1" });
+  try {
+    const input = {
+      CiphertextBlob: Buffer.from(encrypted, "base64"),
+      EncryptionContext: { LambdaFunctionName: functionName },
+      EncryptionAlgorithm: "SYMMETRIC_DEFAULT",
+    };
+    console.log("Attempting to decrypt", envVariable, " with", input);
+    const command = new DecryptCommand(input);
+    const response = await kmsClient.send(command);
+    return new TextDecoder().decode(response.Plaintext);
+  } catch (err) {
+    console.log("Decrypt error:", err);
+    throw err;
+  }
+};
+
+export { decrypt_env_var };
